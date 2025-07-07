842a45ee9fbd45ec4cca4c81288130e081a53173
Mihir72999 <mihir72999@gmail.com>
2025-07-07 08:33:35 +0530

    Third Commit

--- index.js
+++ index.js
@@ -8,15 +8,20 @@ app.use(cors({origin:'*'}))
 app.get('/',async(req ,res)=>{
  const api = 'pt_62oy1a0vq0o6y7e4hqw7thv4bfmilgnr'
  const email = 'theovandepol@gmail.com'
- const author = Buffer.from(`${email}:${api}`).toString('base64')   
-const data = await axios.get('https://api.ownerrez.com/v2/properties',{
-    headers:{
-        'Authorization':`Basic ${author}`,
-        'Content-Type':'application/json',
-        'Accept':'application/json'
+ try {
+    
+     const author = Buffer.from(`${email}:${api}`).toString('base64')   
+     const data = await axios.get('https://api.ownerrez.com/v2/properties',{
+         headers:{
+             'Authorization':`Basic ${author}`,
+             'Content-Type':'application/json',
+             'Accept':'application/json'
+            }
+        })
+        res.json(data.data)
+    } catch (error) {
+       res.status(500).json({message:"went Something Worng" , error})
     }
-})
-res.json(data.data)
-})
+    })
 
 app.listen(3000 , ()=>console.log('server running'))
